f69650ff5d39ace4fb12be15a05674021c88e91e
raquelcps <rodriguez.reads@gmail.com>
2015-06-22 23:45:58 -0500

    #19 Added team player dashboard stats and player profile sidebar to team show page

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -25,7 +25,7 @@ class TeamsController < ApplicationController
 
 
 		 #Team roster - Used teamplayerdashboard instead of commonteamroster to show only players who have played in playoffs
-       po_team_rosters = Unirest.get("http://stats.nba.com/stats/teamplayerdashboard?DateFrom=&DateTo=&GameSegment=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PaceAdjust=N&PerMode=PerGame&Period=0&PlusMinus=N&Rank=N&Season=2014-15&SeasonSegment=&SeasonType=Playoffs&TeamID=#{@team.team_id}&VsConference=&VsDivision=")
+       po_team_rosters = Unirest.get("http://stats.nba.com/stats/teamplayerdashboard?DateFrom=&DateTo=&GameSegment=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PaceAdjust=N&PerMode=Totals&Period=0&PlusMinus=N&Rank=N&Season=2014-15&SeasonSegment=&SeasonType=Playoffs&TeamID=#{@team.team_id}&VsConference=&VsDivision=")
        a = po_team_rosters.body
        b = a["resultSets"]
        c = b[1]
@@ -37,11 +37,34 @@ class TeamsController < ApplicationController
        	hash = Hash[*d.zip(row).flatten]
        	@po_team_rosters_array << hash 
        end
-       # #Teams in playoffs
-       # @po_teams = []
-       # @po_team_touches_array.each do |team|
-       # 	@po_teams << team["TEAM_ID"]
-       # end
+
+       @po_team_roster_playerid = []
+       @po_team_rosters_array.each do |hash|
+            @po_team_roster_playerid << hash["PLAYER_ID"]
+
+       end
+
+      #    @po_player_dashboard_totals = Unirest.get("http://stats.nba.com/stats/playerdashboardbygeneralsplits?DateFrom=&DateTo=&GameSegment=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PaceAdjust=N&PerMode=Totals&Period=0&PlayerID=<%= hash['PLAYER_ID'] %>&PlusMinus=N&Rank=N&Season=2014-15&SeasonSegment=&SeasonType=Playoffs&VsConference=&VsDivision=")
+      #  a = @po_player_dashboard_totals.body
+      # b = a["resultSets"]
+      # c = b[0] #specific array for totals    
+      # d = c["headers"]
+      # e = c["rowSet"]
+
+      # @po_PlayerTotalsdata = []
+      # "ZIPPING!!"
+      # e.each do |row|
+      #   hash = Hash[*d.zip(row).flatten]
+    
+      #   @po_PlayerTotalsdata << hash 
+      # end
+
+     
+#        p "player dashboard"
+# p @po_player_dashboard_totals
+
+
+
 	end
 
 end
